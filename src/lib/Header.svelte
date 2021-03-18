539616a731932d4a622fc15afb936ba76559baba
AlyshaBradley <bradleal02@craighead.school.nz>
2021-03-18 15:12:27 +1300

    Created and styled header

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,4 +1,4 @@
-<header class="hero is-dark-blue">
+<header class="hero is-primary">
     <div class="hero-body">
         <h1 class="title">Header</h1>
         <h2 class="subtitle">Hero subtitle</h2>
---
+++ src/lib/Nav.svelte
@@ -0,0 +1,6 @@
+<nav>
+    <a class="button is-light " href=".">Home</a>
+    <a class="button is-light " href="bridgerton">Bridgerton</a>
+    <a class="button is-light " href="the-witcher">The Witcher</a>
+    <a class="button is-light " href="money-heist">Money Heist</a>
+  </nav>
